c37d77bbe5b89ae78bc985ea5a9b0e581b65edef
krish-17-git <krishkarmakar17@aol.com>
2021-12-13 20:42:55 +0530

    slices added

--- BASIC2/basic2.go
+++ BASIC2/basic2.go
@@ -16,6 +16,7 @@ func main() {
 	//expliicit declaration
 	br := [4]int{2, 4, 1}
 	cr := [...]int{12, 12}
+	cr[0] = 11
 	fmt.Println(br, " and ", cr)
 	fmt.Println(len(br), " and ", len(cr))
 	//declaring array on specific index
@@ -24,4 +25,18 @@ func main() {
 	//multidimentional array
 	ar3d := [3][3][3]int{{}, {}}
 	fmt.Println(ar3d)
+
+	//dynamic arrays ---> slices
+	fixed := [...]int{2, 4}
+	//fixed = append(fixed, 3) --throw compile error not unable to add element
+	fmt.Println(fixed)
+
+	dynamic := []int{5, 6}
+	dynamic = append(dynamic, 4)
+	fmt.Println(len(dynamic), " and ", cap(dynamic))
+	dynamic[0] = 7
+	fmt.Println(dynamic)
+	//slice allocation
+	dynamic2 := make([]int, 8)
+	fmt.Println(dynamic2, " and ", len(dynamic2), " and ", cap(dynamic2))
 }
