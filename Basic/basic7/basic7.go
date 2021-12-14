4fa6419b0b85982589a98b1b4e54e975a405c728
krish-17-git <krishkarmakar17@aol.com>
2021-12-14 12:32:30 +0530

    added basic folder

---
+++ Basic/basic7/basic7.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//interface as generic, only used in custom package ---> https://pkg.go.dev/golang.org/dl/gotip
+	show(1, 2, 3)
+	//show("k", "b")
+}
+
+// func show [T interface{}](output ...T){
+// 	fmt.Println(output)
+// }
+
+func show(integer ...int) {
+	fmt.Println(integer)
+}
